2de047594a12697929109d154b7a24291662b1ca
mubarek78 <shifamubarek@yahoo.com>
2022-03-04 21:12:58 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
+
+
+function makeInt(string) {
+  return parseInt(string);;
+}
+
+console.log(preserveDecimal("10"))
+console.log(makeInt("20"))
